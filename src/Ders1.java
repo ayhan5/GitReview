d56a7d31b3de76df5d5e3be85602f2ffcbc76288
ayhan5 <62219249+ayhan5@users.noreply.github.com>
2020-09-29 22:29:31 -0400

    Iskender bey

--- src/Ders1.java
+++ src/Ders1.java
@@ -3,7 +3,11 @@ public class Ders1 {
 
 	public static void main(String[] args) {
     System.out.println("saat 10.52 am");
-    System.out.println("saat 11.46 am");
+    System.out.println("saat 11.46 am"); 
+    System.out.println("saat 15.07 pm-iskender"); 
+    		
+    		
+		
 	}
 
 }
